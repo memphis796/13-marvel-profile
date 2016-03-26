8fcfc6021188532755961e29858363571f4b48fb
Memphis796 <memphis796@Hotmail.com>
2016-03-26 17:17:59 -0500

    Project is starting to take off

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-'use strict';
+import InfoView from 'info-view';
+
+export default function () {
+  const id = 9906;
+
+  fetch(`http://gateway.marvel.com:80/v1/public/series/${id}?apikey=1a7f07a0041223c5b84268a690293b2e`)
+    .then((res) => res.json())
+    .then((info) => {
+      const series = info.data.results[0];
+      const asidebar = document.querySelector(`.marvel-aside__column`);
+      const infoView = new InfoView(asidebar, series);
+    });
+}
